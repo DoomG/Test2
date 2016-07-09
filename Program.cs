accd2444712c3d66503d9aae4482b0598c7251f0
DoomG <serg.ipatov.si@gmail.com>
2016-07-09 12:18:55 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace ConsoleApplication2
+{
+    public class Test
+    {
+        public string First { get; set; }
+
+        public string Second { get; set; }
+    }
+
+        class Program
+        {
+            private static void Main()
+            {
+                var first = new Test{First = "1", Second = "2"};
+                var second = new Test{First = "1", Second = "2"};
+                Console.WriteLine(first.Equals(second));
+                Console.ReadKey();
+            }
+        }
+    }
+
+
